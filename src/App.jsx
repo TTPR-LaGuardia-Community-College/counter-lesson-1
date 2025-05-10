c2d5fc7809fb0983060eda4d6a2194032f8ffcb9
Tao Su <96sutao@gmail.com>
2025-05-10 14:12:04 -0400

    replaced solution with my own.

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  function checkNumber() {
-    if (count >= 0) {
-      return count
-    } else {
-      // Lets make sure count is set to zero using set state
-      setCount((count) => count = 0)
-      // This set state will cause a re-reneder of the UI calling checkNumber again with the count of 0
-      // Making the (count >= 0) if statement true 
+    return (
+        <>
+            <main>
+                <h1>{count}</h1>
+                <Display count={count} setCount={setCount}/>
+            </main>
+        </>
+    )
+}
+
+
+function Display({count, setCount}) {
+    return (
+        <div>
+            <Button name="increment" count={count} setCount={setCount}/>
+            <Button name="decrement" count={count} setCount={setCount}/>
+        </div>
+    )
+
+}
+
+function Button({ name,setCount }) {
+    const handleClick = () => {
+        if (name === 'increment') {
+            setCount((prevCount) => prevCount + 1)
+        } else {
+            setCount((prevCount) => prevCount === 0 ? prevCount  : prevCount - 1)
+        }
     }
-  }
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h3>count is {checkNumber()}</h3>
-        <button onClick={() => setCount((count) => count + 1)}>
-          +
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          -
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-          {console.log(count)}
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div style={{margin: '20px'}}>
+            <button onClick={handleClick} className="button">
+                {name}
+            </button>
+        </div>
+    )
 }
 
 export default App
